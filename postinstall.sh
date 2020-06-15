c928fc2a515d28898b45f15fa3bb6a358a64a59a
Corvin Tiperciuc <cuvidk@gmail.com>
2020-06-15 10:14:23 +0000

    Further extends the postinstall script and moves the config files inside this repository

--- postinstall.sh
+++ postinstall.sh
@@ -2,7 +2,9 @@
 
 . ./util.sh
 
+GLOBAL_CONFIG_DIR='/etc/conf.d'
 POSTINSTALL_LOG='log.postinstall'
+CONFIG_FILES_URL='https://github.com/cuvidk/config-files'
 
 if [ -t 1 ]; then
     "$0" >$POSTINSTALL_LOG 2>&1
@@ -35,14 +37,27 @@ install_package() {
     pacman -S --noconfirm "$package_name"
 }
 
+configure_vim() {
+    mkdir -p "$GLOBAL_CONFIG_DIR/vim" && \
+    echo '#!/bin/sh' >/etc/profile.d/vim.sh && \
+    echo "alias vim=\"vim -u $GLOBAL_CONFIG_DIR/vim/vimrc\"" >>//etc/profile.d/vim.sh && \
+    chmod +x /etc/profile.d/vim.sh && \
+    cp ./config-files/vim/.vimrc "$GLOBAL_CONFIG_DIR/vim/vimrc"
+}
+
 ################################################################################
 
 PACKAGES="vim \
           man-db \
           man-pages \
           texinfo \
-          wpa_supplicant"
-
+          wpa_supplicant \
+          i3-gaps \
+          i3lock \
+          i3status
+          rxvt-unicode \
+          zsh \
+          "
 
 setup_hostname
 setup_password
@@ -52,3 +67,9 @@ for package in `echo $PACKAGES`;do
     ret=$?
     check_ok $ret "ERR: $package install exit code: $ret. Check $POSTINSTALL_LOG for more information\n"
 done
+
+perform_task configure_vim 'Configuring vim '
+ret=$?
+check_ok $ret "ERR: Configuring vim exit code: $ret. Check $POSTINSTALL_LOG for more information\n"
+
+print_msg 'Done\n'
